39ad63c87278517f9b554121640172c921e79146
FilipeHibrael <filipehibrael27@gmail.com>
2024-01-16 23:55:51 -0300

    venda item

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Resumo from './Pages/Resumo';
 import Sidenav from './Components/Sidenav';
 import Header from './Components/Header';
 import { DataContextProvider } from './Context/DataContext';
+import Vendas from './Pages/Vendas';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <main>
           <Header />
           <Resumo />
+          <Vendas/>
         </main>
       </div>
     </DataContextProvider>
